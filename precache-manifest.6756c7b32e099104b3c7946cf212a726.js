23858230928b0df474ee7469cf06947773af3791
piercecollazo <piercecollazo@gmail.com>
2023-03-08 21:39:28 -0500

    Updates

---
+++ precache-manifest.6756c7b32e099104b3c7946cf212a726.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5bd81b1fd79be327e89faaf480c4ad1",
+    "url": "/personal-site/index.html"
+  },
+  {
+    "revision": "587c736d397d19b97625",
+    "url": "/personal-site/static/css/2.b11108eb.chunk.css"
+  },
+  {
+    "revision": "1cd6d47151ddcdb94e46",
+    "url": "/personal-site/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "587c736d397d19b97625",
+    "url": "/personal-site/static/js/2.809d829a.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/personal-site/static/js/2.809d829a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd6d47151ddcdb94e46",
+    "url": "/personal-site/static/js/main.7542b6c5.chunk.js"
+  },
+  {
+    "revision": "1721100c847bdc27143e",
+    "url": "/personal-site/static/js/runtime-main.b639e5a9.js"
+  }
+]);
