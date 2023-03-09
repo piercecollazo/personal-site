8b05725909725c1e7b15f909a36fefc791e8ec87
piercecollazo <piercecollazo@gmail.com>
2023-03-08 22:13:25 -0500

    image update for Main.js component

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,10 +10,7 @@ export default class Main extends Component {
                         <div className="mx-auto text-center justify-space-between" style={{opacity:'1.0'}}>
                             <h1 className="mx-auto text-white my-0 text-uppercase">Fullstack Web Development and Cloud Engineering</h1>
                             <h2 className="text-white mx-auto mt-2 mb-5">Web development by Alex Collazo</h2>
-                            {/* <a className="btn btn-primary js-scroll-trigger" href="#about">Resume</a>
-                            <a className="btn btn-primary js-scroll-trigger" href="#about">Projects</a>
-                            <a className="btn btn-primary js-scroll-trigger" href="#about">About me</a>
-                            <a className="btn btn-primary js-scroll-trigger" href="#about">Contact</a> */}
+                            <img src='https://api.accredible.com/v1/frontend/credential_website_embed_image/badge/50146648' />
                         </div>
                     </div>
                 </header>
